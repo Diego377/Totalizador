7811e91e0a6a60c1ad9935696652a8e037072f96
DiegoDelgadillo <ddcabrera69@gmail.com>
2021-09-02 09:08:55 -0400

    Tercer req. seleccionar estado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 const cantidad = document.querySelector("#Cantidad-input");
 const form = document.querySelector("#totalizador-form");
 const precio = document.querySelector("#Precio-input");
+const estado = document.querySelector("#estado-input");
 
 
 form.addEventListener("submit", (event) => {
 
     alert("La cantidad indicada: " + cantidad.value);
     alert("El precio indicado: " + precio.value);
+    alert("El estado indicado: " + estado.value);
    
 });
